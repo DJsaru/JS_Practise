0675951841c5216ba81e7238fa799279cfc75589
DJsaru <smartysarangdj@gmail.com>
2023-09-24 17:36:08 +0530

    lo

--- Promise/promise.js
+++ Promise/promise.js
@@ -43,14 +43,14 @@ promisefour.then(function (data) {
 });
 
 const promisefive = new Promise(function (resolve, reject) {
-    let err = false;
-    if(!err){
-        resolve({
-            name: "saru",
-          });
-    }else{
-        reject('Error occur')
-    }
+  let err = false;
+  if (!err) {
+    resolve({
+      name: "saru",
+    });
+  } else {
+    reject("Error occur");
+  }
 });
 promisefive
   .then(function (data) {
@@ -59,21 +59,60 @@ promisefive
   })
   .then(function (username) {
     console.log(username);
-  }).catch(function(err){
-    console.log(err)
-  }).finally(function(){
-    console.log('This is finally block');
   })
+  .catch(function (err) {
+    console.log(err);
+  })
+  .finally(function () {
+    console.log("This is finally block");
+  });
+
+const promisesix = new Promise(function (resolve, reject) {
+  let err = false;
+  if (!err) {
+    resolve({
+      name: "saru",
+    });
+  } else {
+    reject("Error occur");
+  }
+});
+
+async function Consume() {
+  try {
+    const data = await promisesix;
+    console.log(data);
+  } catch (err) {
+    console.log("error");
+  }
+}
+
+// Consume();
+
+async function Consume() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log("Data : ", data);
+  } catch (err) {
+    console.log("errr");
+  }
+}
+
+// Consume();
 
 
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(function(response){
+    return response.json();
+}).then(function(data){
+    console.log(data)
+}).catch(function(){
+    console.log("error")
+})
 
-
-
-
-
-
-//   promises are return values after successfull result 
+//   promises are return values after successfull result
 //   { handling asynchronous operations  }
 
-//   try and catch uses for synchronous operations 
-//   they dont wait for external operations 
+//   try and catch uses for synchronous operations
+//   they dont wait for external operations
